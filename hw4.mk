0bbc8b6c944cf37d88d075631f9c10a0211095ff
saquijano <sa.quijano@uniandes.edu.co>
2018-11-18 10:19:55 -0500

    super simplifique el make, funciona bien

--- hw4.mk
+++ hw4.mk
@@ -1,21 +1,13 @@
-Resultados_hw4.pdf: traye45.pdf iniciales.pdf resultados_hw4.tex
+Resultados_hw4.pdf: resultados_hw4.tex
 	pdflatex Resultados_hw4.tex
 
-traye45.pdf: 45grad.txt  Plots_hw4.py
-	python3 Plots_hw4.py
-
-resultados_hw4.tex: traye45.pdf
-
-45grad.txt: ODE.cpp
-	g++ ODE.cpp -o ODE.out
-	./ODE.out
-
-iniciales.pdf: iniciales.txt Plots_hw4.py
-	python3 Plots_hw4.py
-
-iniciales.txt: PDE.cpp
+resultados_hw4.tex: Plots_hw4.py
 	g++ PDE.cpp -o PDE.out
 	./PDE.out
+	g++ ODE.cpp -o ODE.out
+	./ODE.out
+	python3 Plots_hw4.py
+
 clean:
 	rm *.txt
 	rm *.pdf
