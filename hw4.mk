ad76b39eb497e804047b61c236a5157a810845f0
saquijano <sa.quijano@uniandes.edu.co>
2018-11-18 22:47:41 -0500

    Make ya quedo arreglado

--- hw4.mk
+++ hw4.mk
@@ -1,13 +1,16 @@
-Resultados_hw4.pdf: resultados_hw4.tex Plots_hw4.py
+Resultados_hw4.pdf: resultados_hw4.tex 
 	pdflatex Resultados_hw4.tex
 
-resultados_hw4.tex Plots_hw4.py:
-	g++ PDE.cpp -o PDE.out
-	./PDE.out
+resultados_hw4.tex: Plots_hw4.py 45grad.txt iniciales.txt
+	python3 Plots_hw4.py
+
+45grad.txt: ODE.cpp
 	g++ ODE.cpp -o ODE.out
 	./ODE.out
-	python3 Plots_hw4.py		
 
+iniciales.txt: PDE.cpp	
+	g++ PDE.cpp -o PDE.out
+	./PDE.out
 clean:
 	rm *.out
 	rm *.txt
